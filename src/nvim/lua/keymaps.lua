94142e20906ae9c080825b3641a8ceed4e024fa6
Harald Skoglund <skoglund@gmail.com>
2023-01-29 12:40:56 +0100

    add Ctrl-c to yank to * register

--- src/nvim/lua/keymaps.lua
+++ src/nvim/lua/keymaps.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = ","
 vim.keymap.set("", "<leader>n", ":NvimTreeToggle<CR>")
 vim.keymap.set("", "<C-t>", ":NvimTreeFocus<CR>")
 vim.keymap.set("", "<C-f>", ":NvimTreeFindFile<CR>")
+vim.keymap.set("", "<C-c>", '"*y') -- yank to * register
