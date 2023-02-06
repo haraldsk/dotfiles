7231ee5e6d498dd2f11304a1572377337ac169c0
Harald Skoglund <skoglund@gmail.com>
2023-02-06 15:46:03 +0100

    add some keymaps from neovim-from-scratch

--- src/nvim/lua/my/keymaps.lua
+++ src/nvim/lua/my/keymaps.lua
@@ -1,6 +1,63 @@
-vim.g.mapleader = ","
+local opts = { noremap = true, silent = true }
 
-vim.keymap.set("", "<leader>n", ":NvimTreeToggle<CR>")
-vim.keymap.set("", "<C-t>", ":NvimTreeFocus<CR>")
-vim.keymap.set("", "<C-f>", ":NvimTreeFindFile<CR>")
-vim.keymap.set("", "<C-c>", '"*y') -- yank to * register
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap comma as leader key
+keymap("", ",", "<Nop>", opts)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- NvimTree
+keymap("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
+keymap("n", "<C-t>", ":NvimTreeFocus<CR>", opts)
+keymap("n", "<C-f>", ":NvimTreeFindFile<CR>", opts)
+
+-- Copy & Paste
+keymap("n", "<C-c>", '"*y', opts) -- yank to * register
+
+-- Resize with arrows
+keymap("n", "<A-Up>", ":resize -2<CR>", opts)
+keymap("n", "<A-Down>", ":resize +2<CR>", opts)
+keymap("n", "<A-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<A-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+
+-- Insert --
+--
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Terminal --
+--
