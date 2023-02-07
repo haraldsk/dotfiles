ab88fe127280c0f71d48792becb892a26a56191e
Harald Skoglund <skoglund@gmail.com>
2023-02-07 19:25:07 +0100

    better packer setup

--- src/nvim/lua/my/plugins.lua
+++ src/nvim/lua/my/plugins.lua
@@ -10,8 +10,32 @@ local ensure_packer = function()
   end
   return false
 end
-
+--
 local packer_bootstrap = ensure_packer()
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
