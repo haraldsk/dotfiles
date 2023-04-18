0aed1f94f977d91a3a89111286735b94b7f6224f
Harald Skoglund <skoglund@gmail.com>
2023-04-18 09:26:23 +0200

    trouble: switch to default keymaps

--- src/nvim/lua/my/trouble.lua
+++ src/nvim/lua/my/trouble.lua
@@ -51,5 +51,22 @@ trouble.setup {
     use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 }
 
--- keymap
-vim.keymap.set("n", "<leader>t", ':TroubleToggle<CR>')
+-- keymaps
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  {silent = true, noremap = true}
+)
