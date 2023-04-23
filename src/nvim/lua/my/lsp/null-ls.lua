555e3be70a4de12491a0c27b3bb3304df2435137
Harald Skoglund <skoglund@gmail.com>
2023-04-23 21:02:26 +0200

    add formatters and linters to null-ls

--- src/nvim/lua/my/lsp/null-ls.lua
+++ src/nvim/lua/my/lsp/null-ls.lua
@@ -1,7 +1,9 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
+
+-- https://www.youtube.com/watch?v=b7OguLuaYvE&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ&index=17
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
@@ -9,11 +11,25 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
+  debug = false,
+  sources = {
+    formatting.prettier.with({
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.gofmt.with({ extra_args = { "-s" } }),
+    formatting.goimports,
+    formatting.hclfmt,
+    formatting.markdownlint,
+    formatting.markdown_toc,
+    formatting.stylua,
+    formatting.terraform_fmt,
+    -- diagnostics.cfn_lint,
+    diagnostics.shellcheck,
+    diagnostics.terraform_validate,
+    diagnostics.tfsec,
+    diagnostics.yamllint,
+    diagnostics.vale,
     -- diagnostics.flake8
-	},
+  },
 })
