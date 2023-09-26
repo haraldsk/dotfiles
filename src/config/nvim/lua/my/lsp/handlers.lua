8062667de9ea3aad50b96d3c3efd5977a81e657f
Harald Skoglund <Harald.Skoglund@skatteetaten.no>
2023-09-26 13:52:42 +0200

    update broken nerdtree symbols

--- src/config/nvim/lua/my/lsp/handlers.lua
+++ src/config/nvim/lua/my/lsp/handlers.lua
@@ -11,10 +11,9 @@ M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
   local signs = {
-
     { name = "DiagnosticSignError", text = "" },
     { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
     { name = "DiagnosticSignInfo", text = "" },
   }
 
