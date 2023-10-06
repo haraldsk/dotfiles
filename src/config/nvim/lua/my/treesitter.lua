89f2802b479e7b78ec4f84d77be62d21b2568ce3
Harald Skoglund <Harald.Skoglund@skatteetaten.no>
2023-10-06 18:32:04 +0200

    neovim: setup keyboard shortcuts for treesitter

--- src/config/nvim/lua/my/treesitter.lua
+++ src/config/nvim/lua/my/treesitter.lua
@@ -3,6 +3,10 @@ if not status_ok then
 	return
 end
 
+vim.keymap.set("n", "[c", function()
+  require("treesitter-context").go_to_context()
+end, { silent = true })
+
 configs.setup({
   auto_install = true,
   highlight = {
@@ -14,39 +18,63 @@ configs.setup({
     enable = true,
     disable = { "" }, -- list of language that will be disabled
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+        init_selection = '<CR>',
+        scope_incremental = '<CR>',
+        node_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
+    }
+  },
   textobjects = {
     select = {
       enable = true,
       lookahead = true,
       keymaps = {
-        ["if"] = "@function.inner",
-        ["af"] = "@function.outer",
-        ["ic"] = "@class.inner",
-        ["ac"] = "@class.outer",
-        ["ia"] = "@parameter.inner",
-        ["aa"] = "@parameter.outer",
-        ["il"] = "@loop.inner",
-        ["al"] = "@loop.outer",
+        ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+        ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+        ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+        ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+
+        ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+        ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+        ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+        ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+        ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+        ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+        ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+        ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+        ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+        ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+        ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
         ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-      },
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
-     move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]]"] = { query = "@function.outer", desc = "Next function start" },
-        ["]c"] = { query = "@class.outer", desc = "Next class start" },
-        --
-        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-        ["]o"] = "@loop.*",
+  },
+  swap = {
+    enable = true,
+    swap_next = {
+      ["<leader>a"] = "@parameter.inner",
+    },
+    swap_previous = {
+      ["<leader>A"] = "@parameter.inner",
+    },
+  },
+  move = {
+    enable = true,
+    set_jumps = true, -- whether to set jumps in the jumplist
+    goto_next_start = {
+      ["]]"] = { query = "@function.outer", desc = "Next function start" },
+      ["]c"] = { query = "@class.outer", desc = "Next class start" },
+      --
+      -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+      ["]o"] = "@loop.*",
         -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
         --
         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
@@ -75,7 +103,6 @@ configs.setup({
       goto_previous = {
         ["[d"] = "@conditional.outer",
       }
-    },
     },
   },
   context_commentstring = {
