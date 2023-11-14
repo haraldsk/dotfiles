d2e2c62b85b90d48122986c99d041b30a961991c
Harald Skoglund <Harald.Skoglund@skatteetaten.no>
2023-11-14 08:41:52 +0100

    add nvim-surround plugin

--- src/config/nvim/lua/my/plugins.lua
+++ src/config/nvim/lua/my/plugins.lua
@@ -230,6 +230,22 @@ return require('packer').startup(function(use)
 
   use 'mbbill/undotree'
 
+  use({
+    "kylechui/nvim-surround",
+    config = function()
+        require("nvim-surround").setup({
+        --         Old text                    Command         New text
+        -- -------------------------------------------------------------
+        --    surr*ound_words             ysiw)           (surround_words)
+        --    *make strings               ys$"            "make strings"
+        --    [delete ar*ound me!]        ds]             delete around me!
+        --    remove <b>HTML t*ags</b>    dst             remove HTML tags
+        --    'change quot*es'            cs'"            "change quotes"
+        --    <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+        --    delete(functi*on calls)     dsf             function calls
+        })
+    end
+})
 
   if packer_bootstrap then
     require('packer').sync()
