4c6862b4895c17c4015dd8b255b978592f249677
Harald Skoglund <skoglund@gmail.com>
2024-06-24 10:41:42 +0200

    fix: remove unused inlayhints require

--- src/config/nvim/lua/my/lsp/init.lua
+++ src/config/nvim/lua/my/lsp/init.lua
@@ -3,7 +3,6 @@ if not status_ok then
   return
 end
 
-require "my.lsp.mason"
+require("my.lsp.mason")
 require("my.lsp.handlers").setup()
-require "my.lsp.null-ls"
-require "my.lsp.inlayhints"
+require("my.lsp.null-ls")
