128e9a3c74b2a6c1edb0034c4ba5aaa4e298f8bb
Harald Skoglund <skoglund@gmail.com>
2024-07-11 12:51:35 +0200

    install neogit and update plugins

--- src/config/nvim/lua/my/plugins.lua
+++ src/config/nvim/lua/my/plugins.lua
@@ -74,6 +74,18 @@ local plugin = {
       require("neoclip").setup()
     end,
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+  },
 
   -- folding
   { "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
