8717747b3d18f1d4370f933c785b6bb37a6b383e
Harald Skoglund <skoglund@gmail.com>
2025-11-11 08:50:17 +0100

    fix: migrate LSP config from deprecated lspconfig to vim.lsp.config
    - Remove deprecated require('lspconfig') from mason.lua and init.lua
    - Replace lspconfig[server].setup() with vim.lsp.config()
    - Eliminates nvim-lspconfig deprecation warning for Neovim 0.11+
    - Future-proofs configuration for nvim-lspconfig v3.0.0

--- src/config/nvim/lua/my/lsp/init.lua
+++ src/config/nvim/lua/my/lsp/init.lua
@@ -1,8 +1,3 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
 require("my.lsp.mason")
 require("my.lsp.handlers").setup()
 require("my.lsp.null-ls")
--- src/config/nvim/lua/my/lsp/mason.lua
+++ src/config/nvim/lua/my/lsp/mason.lua
@@ -42,11 +42,6 @@ require("mason-lspconfig").setup({
   automatic_installation = true,
 })
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
-
 local opts = {}
 
 for _, server in pairs(servers) do
@@ -62,5 +57,5 @@ for _, server in pairs(servers) do
     opts = vim.tbl_deep_extend("force", conf_opts, opts)
   end
 
-  lspconfig[server].setup(opts)
+  vim.lsp.config(server, opts)
 end
