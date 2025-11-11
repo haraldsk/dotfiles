1338b7c2cc8ac3ddad353ea91edf57ee96c90551
Harald Skoglund <skoglund@gmail.com>
2025-11-11 19:13:01 +0100

    refactor(nvim): migrate UI components to lazy.nvim plugin specs
    - Migrate bufferline with diagnostics indicators and NvimTree offset
    - Migrate lualine with git info and extensions
    - Migrate gitsigns with keymaps for hunk navigation and staging
    - Migrate todo-comments with jump keymaps
    - Remove old requires from init.lua and mappings/init.lua
    - All plugins use proper lazy loading events

--- src/config/nvim/lua/my/mappings/init.lua
+++ src/config/nvim/lua/my/mappings/init.lua
@@ -6,6 +6,5 @@ end
 require("my.mappings.dap")
 require("my.mappings.general")
 require("my.mappings.telescope")
-require("my.mappings.todo-comments")
 require("my.mappings.treesitter")
 require("my.mappings.unimpaired")
